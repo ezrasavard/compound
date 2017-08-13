5c8769c5978785913a069b688cc56c0f4dd85c50
Ezra Savard <git@ezrasavard.com>
2017-08-12 17:57:10 -0700

    argument parsing for queries

--- compoundfin/core.py
+++ compoundfin/core.py
@@ -1,15 +1,59 @@
-import argparse
+import mapper
 
+import argparse
+import datetime
 import sys
 
-"""
-Interface with user
-Switch between data acquisition or query running modes
-"""
+
+# timestamp generator for argument type
+def timestamp(datestring):
+    return datetime.datetime.strptime(datestring, '%Y-%m-%d')
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(help='Commands')
+
+# TODO: Implement "fetch" command
+# fetch_parser = subparsers.add_parser('fetch',
+#         help='Do the fetchy stuff')
+
+query_parser = subparsers.add_parser('query',
+        help=('Query the database, filter and specify output methods using'
+              ' the command arguments'))
+
+query_parser.add_argument('--start',
+        help='start date (inclusive), YYYY-MM-DD',
+        type=timestamp,
+        dest='START_DATE',
+        required=True)
+
+query_parser.add_argument('--end',
+        help='end date (exclusive), YYYY-MM-DD',
+        type=timestamp,
+        dest='END_DATE',
+        default=datetime.datetime.today())
+
+query_parser.add_argument('--categories',
+        help='space separated list of categories to filter to',
+        nargs='*',
+        choices=mapper.CategoryMap.CATEGORIES)
+
+query_parser.add_argument('--convert-currencies',
+        help=('convert all transactions to one currency at the rate specified'
+              ' in the configuration file'),
+        default="CAD",
+        choices=mapper.Transaction.CURRENCIES)
+
+query_parser.add_argument('--account',
+        help=('Filter to the specified account by name or the final four'
+              ' digits of the account number'))
+
+query_parser.add_argument('--plot',
+        help='Writes data as a pie chart to the specified output file')
+
+query_parser.add_argument('--csv',
+        help='Writes data in CSV format to the specified output file')
 
 
 def run():
-    print("Hello")
-    for arg in sys.argv[1:]:
-        print(arg)
-
+    parser.parse_args(sys.argv[1:])
+    print(parser)
---
+++ tests/test_commands.py
@@ -0,0 +1,55 @@
+from .context import compoundfin
+from compoundfin import core
+
+import datetime
+import unittest
+
+
+class CommandTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.parser = core.parser
+
+    def test_help(self):
+        command = '-h'
+        with self.assertRaises(SystemExit) as e:
+            self.parser.parse_args(command.split())
+        self.assertEquals(e.exception.code, 0)
+
+
+class TestQueryCommand(CommandTest):
+
+    def test_basic_usage(self):
+        command = 'query --start 2016-01-31 --end 2017-01-31'
+        self.parser.parse_args(command.split())
+
+    def test_default_end_date(self):
+        command = 'query --start 2016-01-31'
+        args = self.parser.parse_args(command.split())
+        self.assertEquals(args.END_DATE.date(), datetime.date.today())
+
+    def test_multiple_categories(self):
+        command = 'query --start 2016-01-31 --categories Groceries Healthcare'
+        args = self.parser.parse_args(command.split())
+        self.assertTrue("Groceries" in args.categories)
+        self.assertTrue("Healthcare" in args.categories)
+
+    def test_rejects_unknown_currency(self):
+        command = 'query --start 2016-01-31 --convert-currency RMB'
+        with self.assertRaises(SystemExit) as e:
+            self.parser.parse_args(command.split())
+        self.assertEquals(e.exception.code, 2)
+
+
+class TestFetchCommand(CommandTest):
+
+    def test_basic_usage(self):
+        """
+        This test will fail if "fetch" is implemented and the test cases
+        are not updated
+        """
+        command = 'fetch -h'
+        with self.assertRaises(SystemExit) as e:
+            self.parser.parse_args(command.split())
+        self.assertEquals(e.exception.code, 2)
